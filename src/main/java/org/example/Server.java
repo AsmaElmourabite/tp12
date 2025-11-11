ab67bee32a0d80ff74e6647061fe649353e1eabc
AsmaElmourabite <elmourabiteasma@gmail.com>
2025-11-11 20:14:10 +0100

    Initial commit

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.example.impl.HelloServiceImpl;
+
+public class Server {
+    public static void main(String[] args) {
+        String address = "http://localhost:8080/services/hello";
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        factory.setServiceClass(HelloServiceImpl.class);
+        factory.setAddress(address);
+        factory.create();
+        System.out.println("WSDL: " + address + "?wsdl");
+    }
+}
